c02dbd034daa1a3f45349b630174911a9922c76c
VRomanenko04 <vovasan2004@gmail.com>
2024-05-21 23:48:12 +0300

    Added InfoLine component to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
+'use client';
 import React from 'react';
 import UpperHeader from '../UpperHeader/UpperHeader';
 import LowerHeader from '../LowerHeader/LowerHeader';
+import InfoLine from '../InfoLine/InfoLine';
+import { usePathname } from 'next/navigation';
 
 
 const Header = () => {
+
+    const location = usePathname();
+    const isHomePage = location === '/'
+
     return (
         <header>
+            {isHomePage && <InfoLine />}
             <UpperHeader />
             <LowerHeader />
         </header>
