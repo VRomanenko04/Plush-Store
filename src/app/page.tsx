98742066dd450e0a17eebb13f0330fee439029de
VRomanenko04 <vovasan2004@gmail.com>
2024-05-22 16:19:57 +0300

    Added the Popular block to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HomePageHead from '@/components/HomePageHead/HomePageHead';
 import React from 'react';
 import styles from '@/styles/mainpage.module.scss';
+import HomePagePopular from '@/components/HomePagePopular/HomePagePopular';
 
 export default function MainPage() {
   return (
     <main className={styles.container}>
       <HomePageHead />
+      <HomePagePopular />
     </main>
   );
 }
