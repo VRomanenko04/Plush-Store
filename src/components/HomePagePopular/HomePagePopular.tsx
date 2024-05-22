ea855ba3b555e637793a95eb42c78bba475e0161
VRomanenko04 <vovasan2004@gmail.com>
2024-05-22 16:19:29 +0300

    Wrote a component for popular offers

---
+++ src/components/HomePagePopular/HomePagePopular.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from './HomePagePopular.module.scss';
+import Image from 'next/image';
+import hugCat from '@/assets/hug_cat.jpg';
+import rainbowOctopus from '@/assets/rainbow_octopus.jpg';
+import changelingCat from '@/assets/changeling_cat.jpg';
+import Bag_icon from '@/assets/icons/bag_icon.svg';
+import Link from 'next/link';
+
+const offers = [
+    {
+        title: 'Котик для обіймашок',
+        image: hugCat,
+        price: '500'
+    },
+    {
+        title: 'Різнокольоровий восьминіжок',
+        image: rainbowOctopus,
+        price: '200'
+    },
+    {
+        title: 'Коричневий котик-перекрут',
+        image: changelingCat,
+        price: '180'
+    }
+];
+
+const HomePagePopular = () => {
+    return (
+        <section className={styles.container}>
+            <h2 className={styles.title}>Трендовий плюш</h2>
+            <div className={styles.offers_list}>
+                {offers.map((offer) => (
+                    <div key={offer.title} className={styles.list_item}>
+                        <Image className={styles.image} src={offer.image} alt='image'/>
+                        <h6>{offer.title}</h6>
+                        <div className={styles.price_block}>
+                            <p className={styles.price}>{offer.price} грн</p>
+                            <div className={styles.btn}>
+                                <Image src={Bag_icon} alt='bag icon'/>
+                                У корзину
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <Link href='**' className={styles.link_btn}>
+                Всі пропозиції
+            </Link>
+        </section>
+    )
+}
+
+export default HomePagePopular;
