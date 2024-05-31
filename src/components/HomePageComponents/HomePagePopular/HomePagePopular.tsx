25de219b32664cc3d1e37fbe85f36d277ba07b4a
VRomanenko04 <vovasan2004@gmail.com>
2024-05-31 16:00:19 +0300

    Embedded a modal window on the main page

--- src/components/HomePageComponents/HomePagePopular/HomePagePopular.tsx
+++ src/components/HomePageComponents/HomePagePopular/HomePagePopular.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import styles from './HomePagePopular.module.scss';
 import Image from 'next/image';
 import hugCat from '@/assets/hug_cat.jpg';
 import rainbowOctopus from '@/assets/rainbow_octopus.jpg';
 import changelingCat from '@/assets/changeling_cat.jpg';
 import Bag_icon from '@/assets/icons/bag_icon.svg';
-import Link from 'next/link';
+import LinkButton from '@/components/ui/LinkButton/LinkButton';
+import OrderPopup from '@/components/ui/Popup/OrderPopup/OrderPopup';
 
 const offers = [
     {
@@ -26,28 +28,41 @@ const offers = [
 ];
 
 const HomePagePopular = () => {
+    const [isItemChosen, setIsItemChosen] = useState(false);
+    const [getItemTitle, setGetItemTitle] = useState('');
+
+    const handleItemChoose = (itemTitle: string) => {
+        setIsItemChosen(prev => !prev);
+        setGetItemTitle(itemTitle);
+    }
+
     return (
-        <section className={styles.container}>
-            <h2 className={styles.title}>Трендовий плюш</h2>
-            <div className={styles.offers_list}>
-                {offers.map((offer) => (
-                    <div key={offer.title} className={styles.list_item}>
-                        <Image className={styles.image} src={offer.image} alt={offer.title}/>
-                        <h6>{offer.title}</h6>
-                        <div className={styles.price_block}>
-                            <p className={styles.price}>{offer.price} грн</p>
-                            <div className={styles.btn}>
-                                <Image src={Bag_icon} alt='bag icon'/>
-                                <p>У корзину</p>
+        <>
+            <section className={styles.container}>
+                <h2 className={styles.title}>Трендовий плюш</h2>
+                <div className={styles.offers_list}>
+                    {offers.map((offer) => (
+                        <div key={offer.title} className={styles.list_item}>
+                            <Image className={styles.image} src={offer.image} alt={offer.title}/>
+                            <h6>{offer.title}</h6>
+                            <div className={styles.price_block}>
+                                <p className={styles.price}>{offer.price} грн</p>
+                                <div className={styles.btn} onClick={() => handleItemChoose(offer.title)}>
+                                    <Image src={Bag_icon} alt='bag icon'/>
+                                    <p>У корзину</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-            <Link href='**' className={styles.link_btn}>
-                Всі пропозиції
-            </Link>
-        </section>
+                    ))}
+                </div>
+                <LinkButton editionalClass={styles.link_btn} link='##' btnText='Всі пропозиції'/>
+            </section>
+            <OrderPopup 
+                isOpen={isItemChosen}
+                setisOpen={setIsItemChosen}
+                itemTitle={getItemTitle}
+            />
+        </>
     )
 }
 
